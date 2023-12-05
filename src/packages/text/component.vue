0e1108d9b288b9f4544788b8ee930ee09efbc6f2
bucai <1450941858@qq.com>
2023-12-05 11:16:16 +0800

    feat: text component

--- src/packages/text/component.vue
+++ src/packages/text/component.vue
@@ -1,8 +1,19 @@
 <template>
-  <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
-    <van-swipe-item>1</van-swipe-item>
-    <van-swipe-item>2</van-swipe-item>
-    <van-swipe-item>3</van-swipe-item>
-    <van-swipe-item>4</van-swipe-item>
-  </van-swipe>
+  <div class="tet-comp">
+    {{ text || '请配置' }}
+  </div>
 </template>
+
+<script lang="ts" setup>
+defineProps<{
+  text?: string;
+}>();
+</script>
+
+<style scoped lang="scss">
+.tet-comp {
+  padding: 12px;
+  text-align: center;
+  color: #333;
+}
+</style>
--- src/packages/text/config.ts
+++ src/packages/text/config.ts
@@ -1,2 +1,21 @@
-export default{
+import { Rule } from "@form-create/element-ui";
+
+export default {
+  name: '文本',
+  icon: 'el-icon-document',
+
+  forms: [
+    {
+      type: "input",
+      field: "text",
+      title: "文本",
+      validate: [
+        { type: "string", required: true, message: "请输入标题" },
+      ],
+      props: {
+        placeholder: "请输入标题",
+      },
+    },
+      
+  ] as Rule[],
 }
